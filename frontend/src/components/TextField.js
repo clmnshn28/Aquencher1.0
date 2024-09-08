473ab44d24acea4140426b11377d76ea281fb348
Celmin Shane Quizon <celminshanequizon@gmail.com>
2024-09-09 01:39:06 +0800

    Update Inventory Page Layout

--- frontend/src/components/TextField.js
+++ frontend/src/components/TextField.js
@@ -1,9 +1,9 @@
 import React from "react";
 import 'assets/css/components';
 
-export default function TextField ({onChange, value, label, id, isRequired, type, isReadOnly, isDisabled }){
+export default function TextField ({onChange, value, label, id, isRequired, type, isReadOnly, isDisabled, isInline }){
   return(
-    <div className="TextField__container">
+    <div className={`TextField__container ${isInline ? 'TextField__inline' : ''}`}>
       <label className='TextField__label' htmlFor={id}>{label}
         {isRequired ? <span className="TextField__required-field" > * </span> : ''}
       :
